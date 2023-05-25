dd29b8aeeda9fadb3dc455028ee5cbb1a010acf0
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2023-05-25 21:47:04 +0200

    changed the site to have a home page with some explanation and instead link to the deck generator

---
+++ src/DeckGenerator/DeckGenerator.svelte
@@ -0,0 +1,187 @@
+<script lang="ts">
+  import Deck from "./Deck.svelte";
+  import jsPDF from 'jspdf';
+
+  let decks: Deck[] = [];
+  let titleInput = "";
+  let fileName = "";
+  let includeBacks = false;
+  let fileInput;
+
+  function AddCard(name: string, deck: DeckInterface) {
+    if (name === "") return;
+    deck.cards = [...deck.cards, name];
+    decks[decks.indexOf(deck)] = deck;
+  }
+  function RemoveCard(card, deck: DeckInterface) {
+    deck.cards = deck.cards.filter(x => x != card);
+    decks = decks;
+  }
+
+  function AddDeck(title: string) {
+    if (title === "") return;
+    decks = [...decks, {cards: [], title: title}]
+  }
+  function RemoveDeck(deck: DeckInterface) {
+    decks = decks.filter(x => x !== deck)
+  }
+
+  type DeckInterface = {
+    title: string;
+    cards: string[];
+  }
+
+  function ExportJson(name: string) {
+    if (name === "") return;
+    if (decks.length === 0) return;
+    decks.forEach((x: Deck) => {
+      if (x.cards.length === 0) return;
+    })
+    const dataStr = JSON.stringify(decks);
+    const dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(dataStr);
+    const downloadLink = document.createElement('a');
+    downloadLink.href = dataUri;
+    downloadLink.download = fileName+'.json';
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  }
+
+  function ImportJson() {
+    const reader = new FileReader();
+    reader.onload = () => {
+      try {
+        decks = JSON.parse(reader.result)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    reader.readAsText(fileInput.files[0]);
+  }
+  function ExportPDF(name: string) {
+    if (name === "") return;
+    if (decks.length === 0) return;
+    decks.forEach((x: Deck) => {
+      if (x.cards.length === 0) return;
+    })
+
+    const doc = new jsPDF({format: [300, 400]});
+
+    for (let i = 0; i < decks.length; i++){
+      let deck = decks[i];
+      // Add a new page
+      if (i != 0) doc.addPage();
+
+      let x = 0;
+      let y = 0;
+      let backLength = 0;
+      for (let j = 0; j < deck.cards.length; j++) {
+        const cell = {
+          x: x*75,
+          y: y*100,
+          w: 75,
+          h: 100,
+          text: deck.cards[j],
+          align: 'center', // Center the text within the cell
+          valign: 'middle', // Center the cell vertically within the row
+          lineWidth: 0.1,
+        };
+        doc.rect(cell.x, cell.y, cell.w, cell.h);
+        const lines = doc.splitTextToSize(cell.text, cell.w - 10);
+        const lineHeight = doc.internal.getLineHeight() / doc.internal.scaleFactor;
+        const textHeight = lines.length * lineHeight;
+        const startY = cell.y + (cell.h - textHeight) / 2 + lineHeight / 2;
+        doc.text(cell.x + cell.w / 2, startY, lines, null, null, 'center');
+        backLength++;
+        x++;
+        if (x > 3) {
+          x = 0;
+          y ++;
+          if (y > 3) {
+            y = 0;
+            doc.addPage();
+            if (includeBacks) {
+              AddBacks(doc, deck.title, backLength)
+              if (j != deck.cards.length-1) {
+                doc.addPage()
+              }
+              backLength = 0;
+            }
+          }
+        }
+      }
+      if (includeBacks) {
+        doc.addPage();
+        AddBacks(doc, deck.title, backLength)
+        backLength = 0;
+      }
+    }
+
+    doc.save(name + '.pdf');
+  }
+
+  function AddBacks(doc, title: string, length: number) {
+    let x = 3;
+    let y = 0;
+    for (let i = 0; i < length; i++) {
+      const cell = {
+        x: x*75,
+        y: y*100,
+        w: 75,
+        h: 100,
+        text: title,
+        align: 'center', // Center the text within the cell
+        valign: 'middle', // Center the cell vertically within the row
+        lineWidth: 0.1,
+      };
+      doc.rect(cell.x, cell.y, cell.w, cell.h);
+      doc.text(cell.x + cell.w / 2, cell.y + cell.h / 2, cell.text, null, null, 'center');
+
+      x--;
+      if (x < 0) {
+        x = 3;
+        y ++;
+      }
+    }
+  }
+
+</script>
+<main>
+  <div class="deckInput">
+    <input bind:value={titleInput}>
+    <button on:click={() => AddDeck(titleInput)}>Add deck</button>
+    <button on:click={() => decks = []}>Remove decks</button>
+    <div></div>
+    <input bind:value={fileName}>
+    <div>
+      Include backs
+      <input bind:value={includeBacks} type="checkbox">
+    </div>
+    <button on:click={() => ExportJson(fileName)}>Export deck as JSON</button>
+    <button on:click={() => ExportPDF(fileName)}>Export deck as PDF</button>
+    <input bind:this={fileInput} type="file">
+    <button on:click={() => ImportJson()}>Import deck from JSON</button>
+  </div>
+  <div class="decks">
+    {#each decks as deck}
+      <Deck deck={deck} AddCard={AddCard} RemoveCard={RemoveCard} RemoveDeck={RemoveDeck}/>
+    {/each}
+  </div>
+</main>
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+  }
+  .deckInput {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    padding: 8px;
+    align-items: center;
+  }
+  .decks{
+    display: flex;
+    flex-direction: column;
+    padding: 8px;
+  }
+</style>
---
+++ src/Home/Home.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {Link} from "svelte-routing";
+</script>
+<main>
+    <h1>Cyberethics</h1>
+    <p>Cyberethics refers to the deep and complex ethical questions we have to ask ourselves that come along with the usage of advanced computer technologies. This can range from security concerns to accessibility and discrimination. This project, and in turn this website, is meant to make an attempt at helping educators others with making people aware of the different aspects of the complex topic that is cyberethics. Currently this website hosts two games meant to facilitate this.
+    </p>
+    <h3>Cards for humanity(?)</h3>
+    <p>
+        This is a card game where one player takes on the role of a persona, and the others use cards and their own creativity to come up with products to help them and weaknesses to put the other ideas down.
+
+        The game is meant to be played with a deck of personas fitted to a certain domain, like elder care, personal-economy or social media, where the cards are meant to represent the eventual end users who will actually end up using the product. The technologies can be relatively general, to allow the players to be creative with their ideas, while the weaknesses are logical to cater to the domain itself to stay relevant to the discussion.
+
+        To help make this modifiable to different use cases, a tool for building these decks and exporting them to PDF for easy printing is available on this website. There are also some default decks available.
+    </p>
+    <Link to="/deck-generator">Play here!</Link>
+    <h3>Business simulator</h3>
+    <p>
+        This is a classroom/conference game where players are divided into groups and huddle around a computer and connect with their phones to vote on what response to choose when the companies your team is working as consultants for happen upon ethical dilemmas.
+
+        The game has a central hub set up by the host, where they pick the problem set for the session, and then a group hub which each team joins with one computer. Then each team member joins the group session on their phone with the phone client. All clients are hosted on this webpage.
+    </p>
+    <nav>
+        <Link>Central hub</Link>
+        <Link>Group hub</Link>
+        <Link>Phone client</Link>
+    </nav>
+</main>
